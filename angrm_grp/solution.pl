e92eb3ffb456a0f5e448938cbf929fe8422beb90
Diana Markova <34967260+HeavyHelium@users.noreply.github.com>
2023-04-07 09:49:12 +0300

    add solution

---
+++ angrm_grp/solution.pl
@@ -0,0 +1,28 @@
+
+
+
+app([], L, L).
+app([H | T], L, [H | R]) :- app(T, L, R).
+
+mem(X, [X | _]).
+mem(X, [H | T]) :- x \= H, mem(X, T).
+
+insert(X, L, R) :- app(A, B, L), app(A, [X | B], R).
+
+perm([], []).
+perm([H | T], R) :- perm(T, P), insert(H, P, R).
+
+
+
+insert_in_classes(X, L, R) :- app(A, [C | B], L), 
+                              perm(X, P),
+                              mem(P, C),
+                              app(A, [[X | C] | B], R).
+
+insert_in_classes(X, L, [[X] | L]) :- not((app(_, [C | B], L), 
+                                           perm(X, P),
+                                           mem(P, C))).
+
+group([], []).
+group([H | T], R) :- group(T, R1), 
+                     insert_in_classes(H, R1, R).
---
+++ angrm_grp/task.md
@@ -0,0 +1,17 @@
+This question was asked by Ribinhood. 
+
+Given an array od string, group anagrams together.  
+
+For example,  given the following atrray:   
+```
+'eat', 'ate', 'apt', 'pat', tea, 'now'
+```
+
+Return\:
+
+
+```
+[['eat', 'ate', 'tea'], 
+ ['apt', '[pat]'], 
+ [now]]
+```
