137011a058936cc605c5051eef80ce27e5e16bae
Diana Markova <34967260+HeavyHelium@users.noreply.github.com>
2023-07-18 20:28:52 +0300

    b

--- rand_gen_pr/sol.r
+++ rand_gen_pr/sol.r
@@ -1,5 +1,8 @@
+sim.r <- function(n) {
+  numbers <- c(1, 2, 3, 4)
+  probs <- c(0.1, 0.5, 0.2, 0.2)
+  sample(numbers, size = n, 
+         replace = FALSE, prob = probs)
+}
 
-numbers <- c(1, 2, 3, 4)
-probs <- c(0.1, 0.5, 0.2, 0.2)
-sample(numbers, size = 1, 
-       replace = TRUE, prob = probs)
+table(replicate(10000, sim.r(1)))
